d8b7345e6d12a3d1ae8b36e93c5933de788a4fde
mackenzielewerke <mackenzielewerke@gmail.com>
2017-04-27 23:29:54 -0400

    Updated Index page Updated Portfolio page Layout page with nav-bar working, edited, and responsive Added Images Added Skills page Added Skills Controller and updated Portfolio page

--- Portfolio/Controllers/SkillsController.cs
+++ Portfolio/Controllers/SkillsController.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
 
 namespace Portfolio.Controllers
 {
-    public class PortfoliosController : Controller
+    public class SkillsController : Controller
     {
-        [HttpGet("~/portfolio")]
-        public IActionResult Portfolio()
+        [HttpGet("~/skills")]
+        public IActionResult Skills()
         {
             return View();
         }
